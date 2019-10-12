8538bd7ac5d6fcd9bb27dec054b561e88010db00
aravjain63 <44543506+aravjain63@users.noreply.github.com>
2019-10-12 16:32:13 +0530

    .

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
-from datetime import datetime
+
 
 
 
