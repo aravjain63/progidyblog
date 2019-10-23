125cf1884d6d81d2554ca659c6fc0136b45518db
aravjain63 <aravjain63@gmail.com>
2019-10-23 22:54:12 +0530

    nm

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -5,17 +5,17 @@ from django.utils import timezone
 from datetime import datetime
 
 class Post(models.Model):
-    author_name = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author_name = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    published_date =  models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(default=timezone.now)
     #email = models.models.CharField(_("enter your email"), max_length=50)
     def __str__(self):
         return self.title
         
     def publish(self):
-        self.published_date= timezone.now()
+        self.published_date = timezone.now
         self.save()
 
 #Class Email(models.Model):
