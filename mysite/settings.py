4b473275161556ed02ad100b851299a76f3e6a7e
Dewansh Rawat <dewanshrawat15@gmail.com>
2019-11-01 09:33:21 +0530

    Updating settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [ 
-    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
