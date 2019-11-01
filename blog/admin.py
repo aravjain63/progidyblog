76ebb733b2b1765b43b854e9386e69953ce06488
Dewansh Rawat <dewanshrawat15@gmail.com>
2019-11-01 09:29:30 +0530

    Updating files

---
+++ blog/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib import admin
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,      {'fields': ['author_name']}),
+        (None,      {'fields': ['title']}),
+        ('text', {'fields': ['text']}), 
+        ('Date information', {'fields': ['created_date', 'published_date'], 'classes': ['collapse']}),
+    ]
+    list_display = ('title', 'published_date')
+    search_fields = ['title', 'published_date']
+admin.site.register(Post, PostAdmin)
+
---
+++ blog/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+from datetime import datetime
+
+class Post(models.Model):
+    author_name = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(default=timezone.now)
+    #email = models.models.CharField(_("enter your email"), max_length=50)
+    def __str__(self):
+        return self.title
+        
+    def publish(self):
+        self.published_date = timezone.now
+        self.save()
+
+#Class Email(models.Model):
+    #author_id = models.ForeignKey(Post,on_delete=models.CASCADE)
+
+
+
+       
+
+# Create your models here.
